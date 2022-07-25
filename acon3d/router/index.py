5ca66ee021bf43c1417da0d30887868792dbc52b
naknakLEE <in5986@naver.com>
2022-07-26 00:16:49 +0900

    ADD: init router

--- acon3d/router/index.py
+++ acon3d/router/index.py
@@ -3,7 +3,18 @@ from typing import Union
 
 router = APIRouter()
 
-
 @router.get("/")
 def read_root():
+    return {"Hello": "World"}
+
+@router.post("/product")
+def post_product():
+    return {"Hello": "World"}
+
+@router.get("/product")
+def get_product():
+    return {"Hello": "World"}
+
+@router.put("/product")
+def put_product():
     return {"Hello": "World"}
