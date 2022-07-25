0afe1993a7b4f3a72609c507d3abacda1bc56d96
naknakLEE <in5986@naver.com>
2022-07-26 02:33:35 +0900

    ADD:  get_list_product 추가

--- acon3d/router/index.py
+++ acon3d/router/index.py
@@ -167,7 +167,8 @@ def post_product(
     "status": "검토 요청"
     }
     p_dao = p_dao.append(new_data, ignore_index=True)
-    p_dao = p_dao.reset_index(level=0)
+    p_dao['index'] = p_dao.index
+
     if insert_db(p_dao):
         return {"msg": "상품 등록 성공"}
 
@@ -177,15 +178,14 @@ def get_product(user: str = Depends(get_current_user)):
         return {"msg": "에디터 권한이 없습니다."}
     p_dao = load_product_db()
     p_dao = p_dao.loc[p_dao['status'] == "검토 요청"]
-    print("체크")
-    print(p_dao)
     
     res = {}
     
     rows = []
     
     for index, row in p_dao.iterrows():
-        rows.append({'product_name' : row['product_name'],
+        rows.append({'product_index' : row['index'],
+                    'product_name' : row['product_name'],
                     'title_kr' : row['title_kr'],
                     'content_kr' : row['content_kr'],
                     'user_index' : row['user_index'],
@@ -194,7 +194,77 @@ def get_product(user: str = Depends(get_current_user)):
     return res
 
 @router.put("/product")
-def put_product(user: str = Depends(get_current_user)):
+def put_product(
+                product_index: int,
+                title_kr: str,
+                content_kr: str,
+                title_en: str,
+                content_en: str,
+                title_cn: str,
+                content_cn: str,
+                price: float,
+                tax: float,
+                user: str = Depends(get_current_user),
+                ):
     if user.user_role != '에디터':
         return {"msg": "에디터 권한이 없습니다."}
-    return {"Hello": "World"}
+    p_dao = load_product_db()
+    check_p_dao = p_dao.loc[p_dao['index'] == product_index]
+    if len(check_p_dao) == 0:
+        return{"msg" : "상품이 존재하지 않습니다."}
+    
+    p_dao.at[product_index,'title_kr']=title_kr
+    p_dao.at[product_index,'content_kr']=content_kr
+    p_dao.at[product_index,'title_en']=title_en
+    p_dao.at[product_index,'content_en']=content_en
+    p_dao.at[product_index,'title_cn']=title_cn
+    p_dao.at[product_index,'content_cn']=content_cn
+    p_dao.at[product_index,'price']=price
+    p_dao.at[product_index,'tax']=tax
+    p_dao.at[product_index,'status']="검토 완료"
+    
+    if insert_db(p_dao):
+        return {"msg": "상품 등록 성공"}
+    else:
+        return {"msg": "상품 업데이트 실패"}
+    
+    
+    
+import json
+import simplejson
+
+from fastapi.encoders import jsonable_encoder
+import orjson
+
+# class ORJSONResponse(JSONResponse):
+#     media_type = "application/json"
+
+#     def render(self, content) -> bytes:
+#         return orjson.dumps(content)
+
+import numpy as np
+@router.get("/list/product")
+def get_list_product():
+    p_dao = load_product_db()
+    p_dao = p_dao.loc[p_dao['status'] == "검토 완료"]
+    
+    res = {}
+    
+    rows = []
+    
+    for index, row in p_dao.iterrows():
+        row = row.replace(np.nan,0)
+        rows.append({'product_index' : row['index'],
+                    'product_name' : row['product_name'] if row['product_name'] else None,
+                    'title_kr' : row['title_kr'] if row['title_kr'] else None,
+                    'content_kr' : row['content_kr'] if row['content_kr'] else None,
+                    'title_en' : row['title_en'] if row['title_en'] else None,
+                    'content_en' : row['content_en'] if row['content_en'] else None,
+                    'title_cn' : row['title_cn'] if row['title_cn'] else None,
+                    'content_cn' : row['content_cn'] if row['content_cn'] else None,
+                    'price' : row['price'] if row['price'] else 0,
+                    'tax' : row['tax'] if row['tax'] else 0,
+                    'user_index' : row['user_index'] if row['user_index'] else 0,
+                    'status' : row['status'],})
+    res["msg"] = rows
+    return res
